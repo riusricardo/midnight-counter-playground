1e1430e9c749f5064dcf13e6299df948a639a5f0
riusricardo <ricardo@parity.io>
2025-06-19 13:18:32 -0600

    Fix cjs warning

--- packages/counter-api/tsup.config.ts
+++ packages/counter-api/tsup.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "tsup";
 
+// Let's simplify without plugins
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  format: ["esm", "cjs"], // ESM and CJS formats
+  outDir: "dist",
+  target: "es2020", // Target modern JavaScript
+  platform: "node", // Node.js platform
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
+  // Use noExternal to inline path-resolver module
+  noExternal: ["./src/path-resolver.ts"]
 });
