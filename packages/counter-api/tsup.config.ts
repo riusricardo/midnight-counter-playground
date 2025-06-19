63f422d41b965a45f961b952ccc578f04701796d
riusricardo <ricardo@parity.io>
2025-06-19 13:02:05 -0600

    FIx warnings

--- packages/counter-api/tsup.config.ts
+++ packages/counter-api/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  format: ["esm"], // Only output ESM to support import.meta
   dts: true,
   splitting: false,
   sourcemap: true,
